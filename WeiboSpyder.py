021ea5f087627fb2752596d999c6983c43a48bd2
bugfree314 <dateatom@gmail.com>
2021-11-18 22:06:41 +0800

    Add files via upload

---
+++ WeiboSpyder.py
@@ -0,0 +1,265 @@
+import Weiboutils as WBapi
+
+
+class Comment:
+    """
+    use uid and mid to init
+
+    attrs: uid,mid,comments
+    """
+
+    def __init__(self, uid, mid) -> None:
+        self._uid = uid
+        self._mid = mid
+
+    @property
+    def raw(self):
+        return WBapi.get_comment(self._uid, self._mid)
+
+    @property
+    def comment(self):
+        return [{
+            'uid': c['user']['id'],
+            'name':c['user']['name'],
+            'created_at':c['created_at'],
+            'text':c['text'],
+            'text_raw':c['text_raw']
+        } for c in self.raw]
+
+
+class User:
+    """
+    use uid or user-page-url to init
+
+    attrs: uid,info,shortinfo,fans,follows
+            获取 fans 和 follows 为获取全部，量较大时很慢，取决于网速。
+            可用 Weiboutils 中的 api 获取指定 num。
+    methods: get_Weibo
+    """
+
+    def __init__(self, arg: 'int|str') -> None:
+        try:
+            uid = int(arg)
+        except Exception:
+            uid = WBapi.get_uid_from_url(arg)
+        self.uid = uid
+        self.info = self._get_user_info_(self.uid)['data']['user']
+        shortinfo_keys = ('id', 'screen_name', 'profile_url',
+                          'followers_count', 'friends_count')
+        self.shortinfo = {k: self.info[k] for k in shortinfo_keys}
+
+    def get_Weibo(self, pages=3):
+        return [Weibo(w) for w in WBapi.get_user_weibo(self.uid, pages)]
+
+    @property
+    def fans(self):
+        return WBapi.get_user_follow(
+            self.uid, 1, self.info['data']['user']['followers_count'])
+
+    @property
+    def follows(self):
+        return WBapi.get_user_follow(
+            self.uid, 0, self.info['data']['user']['friends_count'])
+
+    def _get_user_info_(self, uid):
+        return WBapi.get_user_info(uid)
+
+
+class Weibo:
+    """
+    attrs: raw,user,createdtime,text,media,retweet
+    """
+
+    def __init__(self, weibo_dict: dict) -> None:
+        self.raw = weibo_dict
+
+    @property
+    def user(self):
+        if 'visible' in self.raw.keys():
+            url = 'https://weibo.com'+self.raw['user']['profile_url']
+        else:
+            url = self.raw['content']['info']['url']
+        return User(url)
+
+    @property
+    def createdtime(self):
+        if 'visible' in self.raw.keys():
+            return WBapi.dt.datetime.strptime(self.raw['created_at'], WBapi.time_pat).isoformat()
+        else:
+            return self.raw['content']['time']
+
+    @property
+    def text(self):
+        if 'visible' not in self.raw.keys():
+            return self.raw['content']['text']
+        else:
+            txt = {
+                'text': self.raw['text_raw'],
+                'raw': self.raw['text']
+            }
+            if WBapi.re.search(r'>展开</span>', txt['raw']):
+                mblogid = self.raw['mblogid']
+                url = "https://weibo.com/ajax/statuses/longtext"
+                params = {
+                    "id": mblogid
+                }
+                headers = {
+                    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:94.0) Gecko/20100101 Firefox/94.0",
+                    "Cookie": WBapi.get_cookies_from_file()
+                }
+                r = WBapi.requests.get(url, headers=headers, params=params)
+                assert r.status_code == 200
+                txt['raw'] = WBapi.json.loads(
+                    r.content)['data']['longTextContent']
+            return txt
+
+    @property
+    def media(self):
+        if 'visible' not in self.raw.keys():
+            return {
+                'video': self.raw['content']['video'],
+                'image': self.raw['content']['image']
+            }
+        else:
+            media_dict = {
+                'video': [],
+                'image': ['https://wx1.sinaimg.cn/large/'+imgid+'.jpg' for imgid in self.raw['pic_ids']]
+            }
+            if 'url_struct' in self.raw.keys():
+                long_url = self.raw['url_struct'][0]['long_url']
+                m = WBapi.re.search(r'fid=(\d+:\d+)', long_url)
+                if m:
+                    video_id = m.group(1)
+                else:
+                    return media_dict
+                url = "https://weibo.com/tv/api/component"
+                params = {
+                    "data": "{\"Component_Play_Playinfo\":{\"oid\":\""+video_id+"\"}}",
+                }
+                headers = {
+                    "Referer": "https://weibo.com/tv/show/"+video_id+"?from=old_pc_videoshow",
+                    "Cookie": WBapi.get_cookies_from_file()
+                }
+                r = WBapi.requests.post(url, params=params, headers=headers)
+                assert r.status_code == 200
+                media_dict['video'] = WBapi.json.loads(
+                    r.content)['data']['Component_Play_Playinfo']['urls']
+            return media_dict
+
+    @property
+    def retweet(self):
+        fid = None
+        if 'visible' not in self.raw.keys():
+            if self.raw['content']['forward']:
+                fid = WBapi.re.search(
+                    r'/([^/]*)\?', self.raw['content']['forward']).group(1)
+        else:
+            if 'retweeted_status' in self.raw.keys():
+                fid = self.raw['retweeted_status']['mblogid']
+
+        if fid:
+            url = "https://weibo.com/ajax/statuses/show"
+            params = {
+                "id": fid
+            }
+            headers = {
+                "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:94.0) Gecko/20100101 Firefox/94.0",
+                "Cookie": WBapi.get_cookies_from_file()
+            }
+            r = WBapi.requests.get(url, params=params, headers=headers)
+            assert r.status_code == 200
+            return Weibo(WBapi.json.loads(r.content))
+        else:
+            return None
+
+    @property
+    def comment(self):
+        if 'visible' not in self.raw.keys():
+            return Comment(self.raw['act']['comment']['uid'], self.raw['act']['comment']['mid'])
+        else:
+            return Comment(self.raw['user']['id'], self.raw['mid'])
+
+
+class WeiboSpyder:
+    """
+    need cookies to init
+
+    methods:
+    get_allGroups
+    get_topicband
+    get_hotband
+    get_hotWeibos
+    search_Weibo
+    """
+
+    def __init__(self, cookies: str) -> None:
+        WBapi.set_cookies(cookies)
+
+    @property
+    def allGroups(self):
+        """
+        获取分组
+
+        return a dict:
+            key:value --> title:{'gid','containerid'}
+        """
+        band, cat = WBapi.get_allGroups()
+        band.update(cat)
+        return band
+
+    @property
+    def topicband(self):
+        """
+        话题榜
+
+        return a list of dict:
+            key:topic,claim:{uid,user},mention,read,summary,category,mid
+        """
+        return WBapi.get_topicband()
+
+    @property
+    def hotband(self):
+        """
+        热搜榜
+
+        return a tuple of dict:
+            0. hotgov:
+                dict:mid,word,url
+            1. band_list:
+                list of dict:
+                    key:word,category,num,mid,onboard_time
+        """
+        return WBapi.get_hotband()
+
+    def hotWeibos(self, title: str = '24小时榜', num=100):
+        """
+        获取不同类别或时段的热门微博
+
+        title:from API get_allGroups
+        num:MAX=400
+
+        return list of Weibo:
+        """
+        return [Weibo(w) for w in WBapi.get_hotWeibos(title, num)]
+
+    def search(self, keyword: str, searchtype: str = 'weibo', num=10, **search_param):
+        """
+        搜索微博
+
+        搜索类型：
+            'weibo','realtime','user','video','topic'
+            (微博图片搜索质量不高，故排除 pic 类型)
+
+        搜索参数：
+            video:(optional) xsort=hot(热门),typeall=1(全部),hasvideo=0|1
+
+        return:list of searchtype
+        """
+        tags = WBapi.search_Weibo_tags(
+            keyword, searchtype, num, **search_param)
+        if searchtype == 'user':
+            return [User(u['url']) for u in [WBapi.parse_userortopic_tag(t, searchtype) for t in tags]]
+        elif searchtype == 'topic':
+            return [WBapi.parse_userortopic_tag(t, searchtype) for t in tags]
+        else:
+            return [Weibo(w) for w in [WBapi.parse_Weibo_tag(t) for t in tags]]
---
+++ Weiboutils.py
@@ -0,0 +1,483 @@
+import re
+import json
+import requests
+from pandas import DataFrame as df
+import datetime as dt
+from bs4 import BeautifulSoup
+from logging import Logger
+
+time_pat = '%a %b %d %H:%M:%S %z %Y'
+cookies_path = './weibo_cookies.txt'
+
+
+def set_cookies(cookies: str):
+    with open(cookies_path, 'w+') as fp:
+        fp.write(cookies)
+
+
+def get_cookies_from_file():
+    with open(cookies_path, 'r') as fp:
+        cfg = fp.read()
+    return cfg
+
+
+def get_hotband():
+    """
+    热搜榜
+
+    return a tuple of dict:
+        0. hotgov:
+            dict:mid,word,url
+        1. band_list:
+            list of dict:
+                key:word,category,num,mid,onboard_time
+    """
+
+    url = "https://weibo.com/ajax/statuses/hot_band"
+    r = requests.get(url)
+    assert r.status_code == 200
+    hotband_raw_data = json.loads(r.content)['data']
+    if 'hotgov' in hotband_raw_data:
+        hotgov = {
+            'mid': hotband_raw_data['hotgov']['mid'],
+            'word': hotband_raw_data['hotgov']['word'],
+            'url': hotband_raw_data['hotgov']['url']
+        }
+    else:
+        hotgov = {}
+    band_list_raw = hotband_raw_data['band_list']
+    band_list = [{
+        'word': item['word'],
+        'category':item['category'],
+        'num':item['num'],
+        'mid':item['mid'],
+        'onboard_time':dt.datetime.fromtimestamp(item['onboard_time']).isoformat()
+    } for item in band_list_raw if 'category' in item]
+    return hotgov, band_list
+
+
+def get_topicband():
+    """
+    话题榜
+
+    return a list of dict:
+        key:topic,claim:{uid,user},mention,read,summary,category,mid
+    """
+    url = 'https://weibo.com/ajax/statuses/topic_band'
+    r = requests.get(url)
+    assert r.status_code == 200
+    topicband_raw_data = json.loads(r.content)['data']['statuses']
+    keys = ('topic', 'mention', 'read', 'category')
+    topicband = [{k: item[k] for k in keys} for item in topicband_raw_data]
+    return topicband
+
+
+def get_allGroups():
+    """
+    获取分组
+
+    return a tuple of dict:
+        榜单分组 group_band，类别分组 group_category
+        key:value --> title:{'gid','containerid'}
+    """
+    url = 'https://weibo.com/ajax/feed/allGroups'
+    headers = {
+        "Cookie": get_cookies_from_file()
+    }
+    r = requests.get(url, headers=headers)
+    assert r.status_code == 200
+    group_raw = json.loads(r.content)['groups'][3:]
+    group_category_raw = group_raw[0]['group']
+    group_band_raw = group_raw[1]['group']
+    group_category = {
+        item['title']: {'gid': item['gid'], 'containerid': item['containerid']}
+        for item in group_category_raw}
+    group_band = {
+        item['title']: {'gid': item['gid'], 'containerid': item['containerid']}
+        for item in group_band_raw}
+    return group_band, group_category
+
+
+def get_hotWeibos(title: str = '24小时榜', num=100):
+    """
+    获取不同类别或时段的热门微博
+
+    title:from API get_allGroups
+    num:MAX=400
+    return: list of json-like dict
+
+    //note:id used to get comments as mid,
+            user id used to get comments as uid//
+    """
+    groups, g1 = get_allGroups()
+    groups.update(g1)
+    gid = groups[title]['gid']
+    cid = groups[title]['containerid']
+    page = 1
+
+    url = "https://weibo.com/ajax/feed/hottimeline"
+    params = {
+        "group_id": gid,
+        "containerid": cid,
+        "extparam": "discover|new_feed",
+        'max_id': page
+    }
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:94.0) Gecko/20100101 Firefox/94.0",
+        "Cookie": get_cookies_from_file(),
+    }
+    hotWeibos_raw = []
+    if num > 400:
+        num = 400
+    while len(hotWeibos_raw) < num:
+        r = requests.get(url, headers=headers, params=params)
+        assert r.status_code == 200
+        hotWeibos_raw += json.loads(r.content)['statuses']
+        page += 1
+    hotWeibos = hotWeibos_raw[:num]
+
+    # dict_keys = ('created_at', 'id', 'mblogid', 'text_raw', 'text',
+    #              'reposts_count', 'comments_count', 'attitudes_count')
+    # user_keys = ('id', 'screen_name', 'profile_url')
+
+    # def dict_add(d1: dict, d2: dict):
+    #     d1.update(d2)
+    #     return d1
+
+    # hotWeibos = [dict_add({k: item[k] for k in dict_keys}, {
+    #                       'user': {k: item['user'][k] for k in user_keys}}) for item in hotWeibos_raw]
+    # for i in range(len(hotWeibos)):
+    #     hotWeibos[i]['created_at'] = dt.datetime.strptime(
+    #         hotWeibos[i]['created_at'], time_pat).isoformat()
+    return hotWeibos
+
+
+def search_Weibo_raw(keyword: str, searchtype: str = 'weibo', page=1, **search_param) -> BeautifulSoup:
+    """
+    搜索微博(原始接口)
+
+    搜索类型：
+        'weibo','realtime','user','video','topic'
+        (微博文章和图片搜索质量不高，故排除 article 和 pic 类型)
+
+    搜索参数：
+        video:(optional) xsort=hot(热门),typeall=1(全部),hasvideo=0|1
+
+    return:
+        html soup
+
+    """
+    types = ('weibo', 'realtime', 'user', 'video', 'topic')
+    assert searchtype in types
+
+    url = 'https://s.weibo.com/'
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:94.0) Gecko/20100101 Firefox/94.0',
+        'Cookie': get_cookies_from_file()
+    }
+    params_dict = {
+        "q": keyword,
+        'page': page
+    }
+    if searchtype == 'realtime':
+        params_dict.update({"rd": "realtime"})
+    if search_param:
+        params_dict.update(search_param)
+    r = requests.get(url=url+searchtype, headers=headers, params=params_dict)
+    assert r.status_code == 200
+
+    soup = BeautifulSoup(r.text, 'lxml')
+    return soup
+
+
+def search_Weibo_tags(keyword: str, searchtype: str = 'weibo', num=10, **search_param) -> list:
+    """
+    搜索微博（中间接口）
+
+    搜索类型：
+        'weibo','realtime','user','video','topic'
+        (微博图片搜索质量不高，故排除 pic 类型)
+
+    搜索参数：
+        video:(optional) xsort=hot(热门),typeall=1(全部),hasvideo=0|1
+
+    return:
+        list of required html tags
+    """
+    soup = search_Weibo_raw(keyword, searchtype, **search_param)
+    m = re.search(r'\d+', str(soup.find('div', class_='m-error')))
+    if m:
+        total_num = int(m.group())
+    else:
+        total_num = 1000000
+    logger = Logger('search_Weibo')
+    if num > total_num:
+        logger.warning(
+            'Search number is too large,reset to MAX= %d' % total_num)
+        num = total_num
+    tags = []
+    page = 1
+
+    while len(tags) < num:
+        soup = search_Weibo_raw(keyword, searchtype, page, **search_param)
+        if searchtype in ('topic', 'user'):
+            tags += soup.findAll('div', class_='card')
+        else:
+            tags += soup.findAll('div', class_='card-wrap', mid=True)
+        page += 1
+    return tags[:num]
+
+
+def parse_Weibo_tag(tag: BeautifulSoup) -> dict:
+    """
+    从每条微博源码提取信息
+    type=weibo,realtime,video时调用
+
+    return:json-like dict
+            {content:{
+                info:{url:,name:},
+                time:,
+                text:{txt,raw},
+                video:{type,url} or None,
+                image:url or None,
+                forward:url or None,
+                }
+             act:{
+                forward:num,
+                comment:{num,mid,uid},
+                    //use mid and uid to get comment json//
+                like:num,
+                }
+             }
+    """
+    content_tag = tag.find(class_='card-feed')
+    act_tags = tag.find(class_='card-act').findAll('li')
+
+    info_tag = content_tag.find(class_='info').find('a', class_='name')
+    time_tag = content_tag.find(class_='from').next.next.next
+    text_tag = content_tag.findAll(
+        class_='txt', attrs={'nick-name': re.compile(r'.*')})[-1]
+
+    video = None
+    image = None
+    forward = None
+
+    media_tag = content_tag.find(class_='media')
+    if media_tag:
+        if media_tag.find('img'):
+            image_tags = media_tag.findAll('img')
+            image = [tag.attrs['src'].replace(
+                'orj360', 'large') for tag in image_tags]
+        else:
+            video_str = media_tag.find('video-player').attrs[':options']
+            pat = re.compile(r'type:\'(?P<type>.*?)\'.*?src:\'(?P<src>.*?)\'')
+            video = pat.search(video_str.replace('\n', '')).groupdict()
+            video['src'] = 'https:'+video['src']
+
+    d = {
+        'content': {
+            'info': {
+                'url': 'https:'+info_tag.attrs['href'],
+                'name': info_tag.attrs['nick-name']
+            },
+            'time': time_tag.strip(),
+            'text': {
+                'text': text_tag.text.strip(),
+                'raw': text_tag.contents
+            },
+            'video': video,
+            'image': image,
+            'forward': forward
+        },
+        'act': {
+            'forward': act_tags[0].text.strip().replace('转发', '0'),
+            'like': act_tags[2].text.strip().replace('赞', '0'),
+            'comment': {
+                'num': act_tags[1].text.strip().replace('评论', '0'),
+                'mid': tag.attrs['mid'],
+                'uid': re.search(r'/(\d+)\?', info_tag.attrs['href']).group(1)
+            }
+        }
+    }
+    if content_tag.find(class_='card-comment'):
+        forward_tag = content_tag.find(
+            class_='card-comment').find(class_='func').find(class_='from')
+        forward = 'https:'+forward_tag.find('a').attrs['href']
+        d['content'].update({'forward': forward})
+    return d
+
+
+def parse_userortopic_tag(tag: BeautifulSoup, type='user') -> dict:
+    """
+    type:
+         用户 user
+            key:url,name,num
+         话题 topic
+            key:name,num
+    return:dict
+    """
+    if type == 'user':
+        d = {
+            'url': 'https:'+tag.find(class_='name').attrs['href'],
+            'name': tag.find(class_='name').text,
+            'num': tag.find(class_='s-nobr').text
+        }
+    else:
+        d = re.search(r'>(?P<name>#.*#)<', str(tag)).groupdict()
+        d.update(re.search(r'>(?P<num>\d.*讨论.*阅读)<', str(tag)).groupdict())
+    return d
+
+
+def get_comment(uid: 'int|str', mid: 'int|str') -> dict:
+    """
+    通过 uid 和 mid 获取评论
+
+    最多获取 100 条(硬编码到 count)，获取过多服务器容易拒绝
+
+    return json-like dict
+    """
+    url = "https://weibo.com/ajax/statuses/buildComments"
+    params = {
+        "id": mid,
+        "is_show_bulletin": "3",
+        "count": "100",
+        "uid": uid
+    }
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:94.0) Gecko/20100101 Firefox/94.0",
+        "Cookie": get_cookies_from_file()
+    }
+    r = requests.get(url, params=params, headers=headers)
+    assert r.status_code == 200
+    return json.loads(r.content)['data']
+
+
+def get_uid_from_url(url: str) -> str:
+    if re.search(r'\d\?ref',url):
+        return re.search(r'/(\d+)\?',url).group(1)
+    if re.search(r'/u/', url):
+        uid = re.search(r'/(\d+)', url).group(1)
+    else:
+        req_url = "https://weibo.com/ajax/profile/info"
+        headers = {
+            "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:94.0) Gecko/20100101 Firefox/94.0",
+            "Cookie": get_cookies_from_file()
+        }
+        if re.search(r'/n/', url):
+            params = {
+                "screen_name": re.search(r'([^/]*)\Z', url).group(1)
+            }
+        else:
+            params = {
+                "custom": re.search(r'([^/]*)\Z', url).group(1)
+            }
+        r = requests.get(req_url, headers=headers, params=params)
+        assert r.status_code == 200
+        uid = json.loads(r.text)['data']['user']['idstr']
+    return uid
+
+
+def get_user_info(uid: 'str|int') -> dict:
+    """
+    return json-like dict
+    """
+    url = "https://weibo.com/ajax/profile/info"
+    params = {
+        "uid": uid
+    }
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:94.0) Gecko/20100101 Firefox/94.0",
+        "Cookie": get_cookies_from_file()
+    }
+    r = requests.get(url, headers=headers, params=params)
+    assert r.status_code == 200
+    return json.loads(r.text)
+
+
+def get_user_follow(uid: 'str|int', flag: '0|1', num: int) -> list:
+    """
+    获取粉丝或关注
+
+    flag：1 粉丝 fans/followers；0 关注 followings
+
+    return list of json-like dict
+    """
+    user_info_dict = get_user_info(uid)
+    num_follower = user_info_dict['data']['user']['followers_count']
+    num_following = user_info_dict['data']['user']['friends_count']
+
+    page = 1
+    url = "https://weibo.com/ajax/friendships/friends"
+    params = {
+        "page": page,
+        "uid": uid
+    }
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:94.0) Gecko/20100101 Firefox/94.0",
+        "Cookie": get_cookies_from_file()
+    }
+    if flag:
+        params.update({"relate": "fans"})
+        maxnum = num_follower
+    else:
+        maxnum = num_following
+
+    logger = Logger('get_user_follow')
+    if num > maxnum:
+        logger.warning('num too large,reset to MAX: %d' % maxnum)
+        num = maxnum
+
+    user_follow_list = []
+    while len(user_follow_list) < num:
+        r = requests.get(url, headers=headers, params=params)
+        assert r.status_code == 200
+        user_follow_list += json.loads(r.text)['users']
+        page += 1
+    return user_follow_list[:num]
+
+
+def get_user_weibo(uid: 'str|int', pages=3) -> list:
+    """
+    获取用户微博
+
+    return list of json-like
+    """
+    page = 1
+    user_weibo_list = []
+
+    url = "https://weibo.com/ajax/statuses/mymblog"
+    params = {
+        "uid": uid,
+        "page": page
+    }
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:94.0) Gecko/20100101 Firefox/94.0",
+        "Cookie": get_cookies_from_file()
+    }
+
+    for page in range(1, pages+1):
+        r = requests.get(url, headers=headers, params=params)
+        assert r.status_code == 200
+        user_weibo_list += json.loads(r.text)['data']['list']
+
+    return user_weibo_list
+
+
+def get_feeds(num=30) -> list:
+    """
+    获取我关注的最新微博
+
+    return list of json-like 
+    """
+    url = "https://weibo.com/ajax/feed/friendstimeline"
+    params = {
+        "list_id": "null",
+        "count": num
+    }
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:94.0) Gecko/20100101 Firefox/94.0",
+        "Cookie": get_cookies_from_file()
+    }
+    r = requests.get(url, headers=headers, params=params)
+    assert r.status_code == 200
+    return json.loads(r.text)['statuses'][:num]
